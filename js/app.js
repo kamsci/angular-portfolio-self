634a9e84335e71bdeb1d17e71c2fc544013d01d8
Krista Calderon <kamsci@gmail.com>
2016-08-02 13:20:43 -0700

    updated URL for git deploy

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ app.config(function($stateProvider, $urlRouterProvider) {
     controller: 'MainCtrl'
   })
   .state('switchIt', {
-    url: '/switchIt',
+    url: 'angular-portfolio-self/#/switchIt',
     templateUrl: 'views/switchIt.html',
     controller: 'SwitchCtrl'
   })
